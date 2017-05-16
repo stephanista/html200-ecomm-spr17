04551de2d537f591fbd13265a54bec47ab671927
stephanista <houser16@uw.edu>
2017-05-15 20:51:50 -0700

    I might cry, I figured it out. Used products array for onclick position. Works beautifully.

--- js/script.js
+++ js/script.js
@@ -5,14 +5,6 @@ function submitEmail() {
   alert("Thank you for adding "  + email + " to our mailing list! We are happy to have you!");
 }
 
-cart = [];
-
-function addCart(){
-  var product = ['.name', '.description', 'price']
-  cart.push(product);
-  console.log(cart);
-}
-
 var products = [
   {
     "name": "Reversible Plaid",
@@ -21,10 +13,16 @@ var products = [
     "imageTitle": "reversible-plaid.jpg"
   },
   {
-    "name": "Wool Cable Knit",
-    "price": 49.99,
-    "description": "Warm yourself with this women's natural cable knit scarf, crafted from 100% Merino wool. Imported.",
-    "imageTitle": "wool-cable.jpeg"
+    "name": "Fringed Plaid",
+    "price": 18.99,
+    "description": "Generously sized, extra soft and featuring a dazzling fringe, this scarf is rendered in a versatile gray, black and white plaid. Expertly beat the cold with style. 100% acrylic.",
+    "imageTitle": "fringed-plaid.jpeg"
+  },
+  {
+    "name": "Multi Color",
+    "price": 22.99,
+    "description": "The Who What Wear Oversize Color-Block Square Scarf is big, bold, and designed to twist and wrap any way you wish. All the colors of the season are harmonized in this oversize accent, so you can adjust to contrast or match your outfit; soft and lush, it’s your stylish standoff against cold AC and unexpected fall breezes. 100% acrylic",
+    "imageTitle": "multi-color.jpeg"
   },
   {
     "name": "Northern Lights",
@@ -39,16 +37,16 @@ var products = [
     "imageTitle": "ombre-infinity.jpg"
   },
   {
-    "name": "Fringed Plaid",
-    "price": 18.99,
-    "description": "Generously sized, extra soft and featuring a dazzling fringe, this scarf is rendered in a versatile gray, black and white plaid. Expertly beat the cold with style. 100% acrylic.",
-    "imageTitle": "fringed-plaid.jpeg"
+    "name": "Ashby Twill",
+    "price": 70.99,
+    "description": "Faribault brings you the Ashby Twill Scarf in Natural. Woven with a 'broken' twill technique, the Ashby Twill Scarf has a slight zigzag texture. Made in USA, this timeless scarf is crafted with luxurious merino wool and finished with heather gray fringe. 100% Merino wool",
+    "imageTitle": "twill.jpg"
   },
   {
-    "name": "Multi Color",
-    "price": 22.99,
-    "description": "The Who What Wear Oversize Color-Block Square Scarf is big, bold, and designed to twist and wrap any way you wish. All the colors of the season are harmonized in this oversize accent, so you can adjust to contrast or match your outfit; soft and lush, it’s your stylish standoff against cold AC and unexpected fall breezes. 100% acrylic",
-    "imageTitle": "multi-color.jpeg"
+    "name": "Wool Cable Knit",
+    "price": 49.99,
+    "description": "Warm yourself with this women's natural cable knit scarf, crafted from 100% Merino wool. Imported.",
+    "imageTitle": "wool-cable.jpeg"
   },
   {
     "name": "Etro Paisley-Print Silk",
@@ -56,10 +54,16 @@ var products = [
     "description": "Luxurious silk scarf with subtle paisley pattern. 100% silk",
     "imageTitle": "etro.png"
   },
-  {
-    "name": "Ashby Twill",
-    "price": 70.99,
-    "description": "Faribault brings you the Ashby Twill Scarf in Natural. Woven with a 'broken' twill technique, the Ashby Twill Scarf has a slight zigzag texture. Made in USA, this timeless scarf is crafted with luxurious merino wool and finished with heather gray fringe. 100% Merino wool",
-    "imageTitle": "twill.jpg"
-  }
-]
+];
+
+cart = [];
+
+function addCart(n){
+  cart.push(products[n]);
+  console.log(cart);
+}
+
+function remCart(n){
+  cart.splice(n, 1);
+  console.log(cart);
+}
